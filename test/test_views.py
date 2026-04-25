9f3c1a00275dea6d4913b80b450c15b1089c9ea4
Mateusz Zalewski <51079402+ZalewskiMateusz@users.noreply.github.com>
2026-04-25 07:38:58 +0200

    Update test_views.py

--- test/test_views.py
+++ test/test_views.py
@@ -11,7 +11,7 @@ class FlaskrTestCase(unittest.TestCase):
     def test_outputs(self):
         rv = self.app.get('/outputs')
         s = rv.data.decode('utf-8')
-        self.assertTrue(','.join(SUPPORTED) in s)
+        self.assertTrue(", ".join(SUPPORTED) in s)
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
